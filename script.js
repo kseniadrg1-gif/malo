69afc658267327fcd925fdcdba5f4a3962993e78
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-05-27 12:08:47 +0300

    5

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".animate");
-  elements.forEach((el, idx) => {
-    setTimeout(() => {
-      el.classList.add("animated");
-    }, idx * 80);
-  });
-});
